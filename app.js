c3c7212f1c657f3c2492667e294bc02ce86a42ee
harshita sethiya <harshita.sethiya252@gmail.com>
2023-12-29 16:27:48 +0530

    commit2

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const http = require('http');
 
-const routes = require('./routes');
 
-console.log(routes.someText);
+const express=require('express');
 
-const server = http.createServer(routes.handler);
+const app=express();
 
-server.listen(3000);
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();//allow the request to continue to the next middleware in the line
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    
+    res.send('<h1>Hello from express</h1>');
+});
+
+// const server = http.createServer(app);
+
+app.listen(3000);
