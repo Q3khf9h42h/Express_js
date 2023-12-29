3ca4c1782dd0c6202c12322534a150bb13c2deec
harshita sethiya <harshita.sethiya252@gmail.com>
2023-12-29 17:26:08 +0530

    commit3

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-
-
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();//allow the request to continue to the next middleware in the line
-});
-app.use((req,res,next)=>{
-    console.log('In another middleware');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+ res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     
-    res.send('<h1>Hello from express</h1>');
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    console.log('In another middleware');
+     res.send('<h1>Hello from express</h1>');
 });
 
 // const server = http.createServer(app);
